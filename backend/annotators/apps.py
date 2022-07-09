c9e042a151af1b5c007657d8c777f6cae77bd6e0
Rahadian Alamsyah <rahadian.apw@gmail.com>
2022-07-10 03:18:18 +0700

    feat: init django project

---
+++ backend/annotators/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AnnotatorsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'annotators'
---
+++ backend/annotators/models.py
@@ -0,0 +1,36 @@
+from email.mime import image
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+class AnnotationProject(models.Model):
+    title = models.CharField('project title', max_length = 100)
+    description = models.TextField('project description')
+    owner = models.ForeignKey(
+      settings.AUTH_USER_MODEL,
+      on_delete=models.CASCADE,
+      related_name='owner'
+    )
+    annotator = models.ManyToManyField(
+      settings.AUTH_USER_MODEL,
+      on_delete=models.CASCADE,
+      related_name='annotator'
+    )
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images')
+    upload_date = models.DateTimeField('date uploaded', auto_now_add=True)
+    project = models.ForeignKey(AnnotationProject)
+
+class ImageAnnotation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(
+      settings.AUTH_USER_MODEL,
+      on_delete=models.CASCADE
+    )
+    # will be following M-RCNN inference result
+    # polygon = models.po
+    # point = 
+    # bounding
